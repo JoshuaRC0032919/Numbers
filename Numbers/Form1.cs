8c6b8089145bd94e36e71c92721dc1b569650d10
Joshua Law Holland <rc0032919@student.riversidecollege.ac.uk>
2020-09-28 15:55:06 +0100

    Numbers equations

--- Numbers/Form1.cs
+++ Numbers/Form1.cs
@@ -15,15 +15,7 @@ namespace Numbers
         public Form1()
         {
             InitializeComponent();
-            double d1 = 5e6;
-            richTextBox1.AppendText(d1.ToString() + "\n");
-            richTextBox1.AppendText(d1.ToString("N") + "\n");
-            richTextBox1.AppendText(d1.ToString("N3")+"\n");
-            richTextBox1.AppendText(d1.ToString("N0") + "\n");
-            richTextBox1.AppendText(d1.ToString("0") + "\n");
-            richTextBox1.AppendText(d1.ToString("0.0") + "\n");
-            richTextBox1.AppendText(d1.ToString("0.0E0") + "\n");
-            richTextBox1.AppendText(d1.ToString("0.00E00") + "\n");
+           
 
             double pi = 3.1415926553589793;
             richTextBox1.AppendText(pi.ToString() + "\n");
@@ -38,26 +30,21 @@ namespace Numbers
                 MessageBox.Show(st + " is not an integer");
             }
 
-            string s2 = "3";
-            try
-            {
-                int i2 = int.Parse(s2);
-            }
-            catch
-            {
-                MessageBox.Show(s2 + " is not an integer");
-                int i2 = 0;
-            }
+            
+            
 
-            string s3 = "pi";
-            try
-            {
-                double d2 = double.Parse(s3);
-            }
-            catch
-            {
-                double d2 = 0;
-            }
+            int i1 = 3 * 4;
+            int i2 = 12 / 4;
+            int i3 = 15 / 4;
+            int i4 = 15 % 4;
+            int i5 = 2 + 3 * 4;
+            int i6 = 2 + (3 * 4) / (2 - 4);
+            double d1 = 3.1 * 2.3;
+            double d2 = 1.0 / 3.0;
+            double d3 = 1 / 3;
+
+            richTextBox1.AppendText(i1 + "\n" + i2 + "\n" + i3 + "\n" + i4 + "\n" + i5 + "\n" + i6 + "\n");
+            richTextBox1.AppendText(d1 + "\n" + d2 + "\n" + d3 + "\n");
         }
 
         private void Form1_Load(object sender, EventArgs e)
